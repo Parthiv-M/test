2a5a4a702d2d4fb0fced2787ebdbcfdee96600ea
Parthiv-M <parthiv.menon@learner.manipal.edu>
2021-10-12 23:08:40 +0530

    added cors

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const port = process.env.PORT || 3001;
 
+app.use(cors())
 app.use(express.json({ extended: false }));
 
 app.use("/api/test", require("./api/test"))
